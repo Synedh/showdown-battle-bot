ad8ab596ccdc8124fa3e106f6096b13968b90063
Denis Dekeyzer <ddekeyzer@aspaway.fr>
2016-12-15 15:45:33 +0100

    Adapted typechart to json. Minor changes

--- login.py
+++ login.py
@@ -15,6 +15,6 @@ async def log_in(websocket, id, chall):
                          })
     print(json.loads(resp.text[1:]))
     await websocket.send("|/trn " + username + ",0," + json.loads(resp.text[1:])['assertion'])
-    print("> |/trn " + username + ",0," + json.loads(resp.text[1:])['assertion'])
+    print(">> |/trn " + username + ",0," + json.loads(resp.text[1:])['assertion'])
     await websocket.send("|/avatar 159")
-    print("> |/avatar 159")
+    print(">> |/avatar 159")
--- main.py
+++ main.py
@@ -8,17 +8,20 @@ from login import log_in
 
 async def stringing(websocket, string_tab):
     if string_tab[1] == "challstr":
+        # Si la challstr a  été encoyée, on peut se connecter
         await log_in(websocket, string_tab[2], string_tab[3])
     elif string_tab[1] == "updateuser" and string_tab[2] == "SuchTestBot":
+        # Si on est loggé, alors on peut commencer les combats
         await websocket.send("|/challenge synedh, gen7randombattle")
     elif "battle" in string_tab[0]:
+        # Si on reçoit un message dans une interfacede combat
         print("Battlelog")
 
 async def main():
     async with websockets.connect('ws://sim.smogon.com:8000/showdown/websocket') as websocket:
         while True:
             message = await websocket.recv()
-            print("< {}".format(message))
+            print("<< {}".format(message))
             await stringing(websocket, message.split('|'))
 
-asyncio.get_event_loop().run_until_complete(main())
+# asyncio.get_event_loop().run_until_complete(main())
