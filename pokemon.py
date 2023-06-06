b3d0a92211af344045e1425a07544cbd26b42ad1
Synedh <contact@synedh.fr>
2023-06-06 15:09:45 +0200

    added team

--- pokemon.py
+++ pokemon.py
@@ -58,3 +58,13 @@ class Pokemon:
 
     def set_status(self, status):
         self.status = status
+
+
+class Team:
+    def __init__(self, p1, p2, p3, p4, p5, p6):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.p5 = p5
+        self.p6 = p6
