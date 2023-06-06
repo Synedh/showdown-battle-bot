fdd061cf397810f0ed44d36aeb58ba885d90b8ed
Synedh <contact@synedh.fr>
2023-06-06 15:09:45 +0200

    Tried to logging in

--- main.py
+++ main.py
@@ -2,11 +2,28 @@
 
 import asyncio
 import websockets
+import requests
+
+async def log_in(websocket, challstr):
+    logfile = open("id.txt")
+    username = logfile.readline()[:-1]
+    password = logfile.readline()[:-1]
+    logfile.close()
+    datastr = "act=login&name=" + username + "&pass=" + password + "&challstr=" + challstr
+    print(challstr[len(challstr) - 1])
+    resp = requests.post("http://play.pokemonshowdown.com/action.php",
+                  data=datastr)
+    print(str(resp.status_code))
+    print(str(resp.headers))
+    print(str(resp.encoding))
+    print(str(resp.text))
+    print(str(resp.json()))
 
 async def stringing(websocket, string_tab):
     if string_tab[1] == "challstr":
-        await websocket.send("|/autojoin lobby")
-        print("> |/join lobby")
+        # await websocket.send("|/join lobby")
+        # print("> |/join lobby")
+        await log_in(websocket, string_tab[3])
 
 async def hello():
     async with websockets.connect('ws://sim.smogon.com:8000/showdown/websocket') as websocket:
