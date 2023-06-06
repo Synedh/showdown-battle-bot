9770aca54f9999cd3e30f239b30db4ae506d50c8
Synedh <contact@synedh.fr>
2023-06-06 15:09:45 +0200

    Added multiple meta gestion. Added monotype random

--- src/io_process.py
+++ src/io_process.py
@@ -9,15 +9,15 @@ nb_fights = 2
 nb_fights_done = 0
 
 
-def check_battle(battle_list, room_id):
+def check_battle(battle_list, battletag):
     """
     Get Battle corresponding to room_id.
     :param battle_list: Array of Battle.
-    :param room_id: Id of Battle.
+    :param battletag: String, Tag of Battle.
     :return: Battle.
     """
     for battle in battle_list:
-        if battle.room_id == room_id:
+        if battle.battletag == battletag:
             return battle
     return None
 
@@ -29,15 +29,16 @@ async def battle_tag(websocket, message):
     """
     global battles
     lines = message.splitlines()
-    battle = check_battle(battles, lines[0].split('-')[len(lines[0].split('-')) - 1])
+    battle = check_battle(battles, lines[0].split("|")[0].split(">")[1])
     for line in lines[1:]:
         try:
             current = line.split('|')
             if current[1] == "init":
                 # Creation de la bataille
-                battles.append(Battle(lines[0].split('-')[len(lines[0].split('-')) - 1]))
-                await senders.sendmessage(websocket, battles[len(battles) - 1].room_id, "Hi")
-                await senders.sendmessage(websocket, battles[len(battles) - 1].room_id, "/timer on")
+                battle = Battle(lines[0].split("|")[0].split(">")[1])
+                battles.append(battle)
+                await senders.sendmessage(websocket, battle.battletag, "Hi")
+                await senders.sendmessage(websocket, battle.battletag, "/timer on")
             elif current[1] == "player" and len(current) > 3 and current[3].lower() == "suchtestbot":
                 # Récupérer l'id joueur du bot
                 battle.set_player_id(current[2])
@@ -59,8 +60,8 @@ async def battle_tag(websocket, message):
             elif current[1] == "callback" and current[2] == "trapped":
                 await battle.make_move(websocket)
             elif current[1] == "win":
-                await senders.sendmessage(websocket, battle.room_id, "wp")
-                await senders.leaving(websocket, battle.room_id)
+                await senders.sendmessage(websocket, battle.battletag, "wp")
+                await senders.leaving(websocket, battle.battletag)
             elif current[1] == "c":
                 # This is a message
                 pass
@@ -76,25 +77,28 @@ async def stringing(websocket, message):
     """
     global nb_fights
     global nb_fights_done
+
     string_tab = message.split('|')
     if string_tab[1] == "challstr":
         # Si la challstr a  été encoyee, on peut se connecter
         await log_in(websocket, string_tab[2], string_tab[3])
-    elif string_tab[1] == "updateuser" and string_tab[2] == "SuchTestBot":
+    elif string_tab[1] == "updateuser" and string_tab[2] == "SuchTestBot" or string_tab[1] == "deinit":
         # Si on est log, alors on peut commencer les combats
         pass
-        # await senders.searching(websocket)
+        # if nb_fights_done < nb_fights:
+        #     await senders.searching(websocket)
+        #     nb_fights_done += 1
         # await senders.challenge(websocket, "Synedh")
     elif "updatechallenges" in string_tab[1]:
         # Si quelqu'un envoie un challenge, alors accepter
         try:
             if string_tab[2].split('\"')[3] != "challengeTo":
-                if string_tab[2].split('\"')[5] == "gen7randombattle":
+                if string_tab[2].split('\"')[5] in ["gen7randombattle", "gen7monotyperandombattle",]:
                     await senders.sender(websocket, "", "/accept " + string_tab[2].split('\"')[3])
                 else:
                     await senders.sender(websocket, "", "/reject " + string_tab[2].split('\"')[3])
                     await senders.sender(websocket, "", "/pm " + string_tab[2].split('\"')[3]
-                                         + ", Sorry, I accept only [Gen 7] Random Battle challenges.")
+                                         + ", Sorry, I accept only Randomized Metas.")
         except KeyError:
             pass
     elif "battle" in string_tab[0]:
--- src/senders.py
+++ src/senders.py
@@ -28,16 +28,17 @@ async def challenge(websocket, player):
     """
     await sender(websocket, "", "/challenge " + player + ", gen7randombattle")
 
-async def sendmessage(websocket, room_id, message):
+async def sendmessage(websocket, battletag, message):
     """
     Format text websocket, call sender function.
     :param websocket: Websocket stream.
     :param room_id: Room name.
     :param message: Message to sent.
     """
-    await sender(websocket, "battle-gen7randombattle-" + room_id, message)
+    # await sender(websocket, "battle-gen7randombattle-" + room_id, message)
+    await sender(websocket, battletag, message)
 
-async def sendmove(websocket, room_id, move, turn):
+async def sendmove(websocket, battletag, move, turn):
     """
     Format move choice websocket, call sender function.
     :param websocket: Websocket stream.
@@ -45,9 +46,10 @@ async def sendmove(websocket, room_id, move, turn):
     :param move: Move id (1, 2, 3, 4).
     :param turn: Battle turn (1, 2, ...). Different from the one sent by server.
     """
-    await sender(websocket, "battle-gen7randombattle-" + room_id, "/choose move " + str(move), str(turn))
+    # await sender(websocket, "battle-gen7randombattle-" + room_id, "/choose move " + str(move), str(turn))
+    await sender(websocket, battletag, "/choose move " + str(move), str(turn))
 
-async def sendswitch(websocket, room_id, pokemon, turn):
+async def sendswitch(websocket, battletag, pokemon, turn):
     """
     Format switch choice websocket, call sender function.
     :param websocket: Websocket stream.
@@ -55,12 +57,14 @@ async def sendswitch(websocket, room_id, pokemon, turn):
     :param pokemon: Pokemon id (1, 2, 3, 4, 5, 6).
     :param turn: Battle turn (1, 2, ...). Different from the one sent by server.
     """
-    await sender(websocket, "battle-gen7randombattle-" + room_id, "/choose switch " + str(pokemon), str(turn))
+    # await sender(websocket, "battle-gen7randombattle-" + room_id, "/choose switch " + str(pokemon), str(turn))
+    await sender(websocket, battletag, "/choose switch " + str(pokemon), str(turn))
 
-async def leaving(websocket, room_id):
+async def leaving(websocket, battletag):
     """
     Format leaving room websocket, call sender function.
     :param websocket: Websocket stream.
     :param room_id: Room name.
     """
-    await sender(websocket, "", "/leave battle-gen7randombattle-" + room_id)
+    # await sender(websocket, "", "/leave battle-gen7randombattle-" + room_id)
+    await sender(websocket, "", "/leave " + battletag)
